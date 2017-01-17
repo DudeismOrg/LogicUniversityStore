fd537efaa2d280bb90c36710e996de40e2283f6e
AHMEDRAAJ\ahmed <ahmedraaj@gmail.com>
2017-01-17 18:24:41 +0800

    Controller and Dao

---
+++ LogicUniversityStore/Controller/ProcessReqController.cs
@@ -0,0 +1,23 @@
+﻿using LogicUniversityStore.Dao;
+using LogicUniversityStore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogicUniversityStore.Controller
+{
+    public class ProcessReqController
+    {
+        public RequisitionDao RequisitionDao { get; set; }
+        public ProcessReqController()
+        {
+            RequisitionDao = new RequisitionDao();
+        }
+
+        public List<Requisition> getApprovedRequisitionList()
+        {
+            RequisitionDao.db
+        }
+    }
+}
