bfc663295fba3ee5b98bfabbff50a6d6d128c705
AHMEDRAAJ\ahmed <ahmedraaj@gmail.com>
2017-01-18 11:36:14 +0800

    ProcessReqController

--- LogicUniversityStore/Controller/ProcessReqController.cs
+++ LogicUniversityStore/Controller/ProcessReqController.cs
@@ -1,5 +1,6 @@
 ﻿using LogicUniversityStore.Dao;
 using LogicUniversityStore.Model;
+using LogicUniversityStore.Util;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +11,33 @@ namespace LogicUniversityStore.Controller
     public class ProcessReqController
     {
         public RequisitionDao RequisitionDao { get; set; }
+        public StockCardDao StockCardDao { get; set; }
         public ProcessReqController()
         {
             RequisitionDao = new RequisitionDao();
+            StockCardDao = new StockCardDao();
         }
 
-        public List<Requisition> getApprovedRequisitionList()
+        public List<Tuple<int, Requisition>> GetMainProcessReqList()
         {
-            RequisitionDao.db
+            List<Tuple<int, Requisition>> mainList = new List<Tuple<int, Requisition>>();
+            foreach (var requisition in RequisitionDao.getApprovedRequisitionList())
+            {
+                int? unfullfilledItem = 0;
+                foreach(RequisitionItem rItem in requisition.RequisitionItems)
+                {
+                    unfullfilledItem = unfullfilledItem + ((rItem.NeededQuantity - StockCardDao.GetProductCountInStock(rItem.SupplierItem.BaseItemID)) > 0 ? 1 : 0);
+                }
+                if(unfullfilledItem.Value == 0)
+                {
+                    requisition.Status = RequisitionStatus.Allocated.ToString();
+                }
+                mainList.Add(new Tuple<int,Requisition>(unfullfilledItem.Value, requisition));
+            }
+
+            return mainList;
+            
         }
+        
     }
 }
