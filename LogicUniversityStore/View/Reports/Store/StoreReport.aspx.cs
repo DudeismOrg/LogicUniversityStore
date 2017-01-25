981b19a901cebcecddca8e7e5142bf4597e0dd9f
Imran <rsvp.imi@gmail.com>
2017-01-25 18:43:25 +0800

    added new items

---
+++ LogicUniversityStore/View/Reports/Store/StoreReport.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LogicUniversityStore.View.Reports.Store
+{
+    public partial class StoreReport : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TEST trprt = new TEST();
+            testReport.ReportSource = trprt;
+            testReport.Zoom(75);
+        }
+
+    }
+}
