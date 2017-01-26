93b7bae754eae5b69e4afe78062a9b0bb343eb1b
NarayananJanaki <janaki.narayanan31@gmail.com>
2017-01-26 09:17:44 +0800

    cancel update pending employee

---
+++ LogicUniversityStore/Controller/CancelUpdatePendingApprovalController.cs
@@ -0,0 +1,36 @@
+﻿using LogicUniversityStore.Dao;
+using LogicUniversityStore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogicUniversityStore.Controller
+{
+    public class CancelUpdatePendingApprovalController
+    {
+        public RequisitionDao RequisitionDao { get; set; }
+        public RequisitionItemDao RequisitionItemDao { get; set; }
+        public CancelUpdatePendingApprovalController()
+        {
+            RequisitionDao = new RequisitionDao();
+            RequisitionItemDao = new RequisitionItemDao();
+        }
+
+        public List<Requisition> getRequestedRequisition()
+        {
+            return RequisitionDao.GetRequestedRequisitionList();
+        }
+
+        public void removeRequisitionItems(int reqId)
+        {
+            RequisitionItemDao.deleteRequisitionItem(reqId);
+        }
+
+        public bool removeRequisition(int reqId)
+        {
+            return RequisitionDao.Remove(reqId);
+        }
+
+    }
+}
