f276714251ae6f94d92dd713f630807dd91df578
NarayananJanaki <janaki.narayanan31@gmail.com>
2017-02-02 17:24:01 +0800

    removinb form

--- LogicUniversityStore/View/Department/Hod/RequestHistory.aspx.designer.cs
+++ LogicUniversityStore/View/Department/Hod/RequestHistory.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace LogicUniversityStore.View.Department.Hod {
     public partial class WebForm4 {
         
         /// <summary>
-        /// requestHistory control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm requestHistory;
-        
-        /// <summary>
         /// gvRequestHistory control.
         /// </summary>
         /// <remarks>
