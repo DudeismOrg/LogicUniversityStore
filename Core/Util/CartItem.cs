408c347fd9d5f8def071d2e09ad8a9ca98ed8782
AHMEDRAAJ\ahmed <ahmedraaj@gmail.com>
2017-02-02 17:43:54 +0800

    Requisition can handle multiple value

--- Core/Util/CartItem.cs
+++ Core/Util/CartItem.cs
@@ -7,11 +7,21 @@ using System.Web;
 namespace LogicUniversityStore.Util
 {
     [Serializable]
-    public class CartItem
+    public class CartItem : IEquatable<CartItem>
     {
         public Category Category { get; set; }
 
         public SupplierItem  SupplierItem { get; set; }
         public int Quantity { get; set; }
+
+        public bool Equals(CartItem other)
+        {
+            return (this.Category.CategoryID == other.Category.CategoryID && this.SupplierItem.ItemID == other.SupplierItem.ItemID);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.SupplierItem.ItemID.GetHashCode();
+        }
     }
 }
